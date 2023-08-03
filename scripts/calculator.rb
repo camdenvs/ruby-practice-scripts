ce8e834c187827f45e1e3563536fd5e5fc06f891
Camden Van Sciver <111025127+camdenvs@users.noreply.github.com>
2023-08-02 18:25:09 -0700

    Update calculator.rb

--- scripts/calculator.rb
+++ scripts/calculator.rb
@@ -1,7 +1,7 @@
 puts "Enter a number"
-num1 = gets.chomp.to_i
+num1 = gets.chomp.to_f
 puts "Enter another number"
-num2 = gets.chomp.to_i
+num2 = gets.chomp.to_f
 puts "Enter '+', '-', '*', or '/'"
 operation = gets.chomp
 
